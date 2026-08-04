efe361cc82b8910ef04e50217bba64034f6f98c7
yotamelimeleh01 <yotame01@gmail.com>
2026-08-03 21:00:53 -0400

    Connect app to Supabase project

--- config.js
+++ config.js
@@ -15,6 +15,6 @@
 // ---------------------------------------------------------------------------
 
 const SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://lunbujlbjufummhrcbzv.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1bmJ1amxianVmdW1taHJjYnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3OTU1MjAsImV4cCI6MjEwMTM3MTUyMH0.vyQComHS46sdCCBTYKB_GaW37yYhXBQ1WDXQB2UE9hE",
 };
